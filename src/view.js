c1de29fb3f621c74c98d6718817e3d0a3585f644
Carsten Bach <mail@carsten-bach.de>
2026-09-16 09:56:16 +0200

    Add some inline docs

--- src/view.js
+++ src/view.js
@@ -3,6 +3,15 @@
  *
  * This file defines the reactive state and actions for the calendar block
  * using the WordPress Interactivity API.
+ *
+ * Progressive enhancement for the GatherPress Calendar block.
+ * This script adds interactive popover functionality to event dots,
+ * but the calendar remains fully functional without JavaScript.
+ *
+ * - Event dots are simple <a> links
+ * - Content (innerBlocks) for each event is in a hidden container
+ * - JavaScript reads from hidden container and shows in popover
+ * - Creates valid HTML
  */
 
 /**
