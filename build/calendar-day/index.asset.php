e354bf86215be150a5313c90de09d2468b4a35a6
Carsten Bach <mail@carsten-bach.de>
2026-09-20 17:27:28 +0200

    DO NOT USE display: contents; on the week class in the editor

--- build/calendar-day/index.asset.php
+++ build/calendar-day/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5faba5a8323168ff7053');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'b3689ac8ef8d1aab0841');
--- build/calendar-week/index.asset.php
+++ build/calendar-week/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '15462bb33a92d539b03f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '73daa63c95987323f4e4');
