0a7b6a9d36c4fff6f890667fbd85134a67f259cf
Carsten Bach <mail@carsten-bach.de>
2026-09-21 18:03:17 +0200

    Remove outdated styles

--- build/calendar/index.asset.php
+++ build/calendar/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '8cecc9e7617dafa05fd7');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-date', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => 'fe9dfced8064daf57502');
