af88b9b054fda4c6b16bcd04eb2e195ad281b242
Carsten Bach <mail@carsten-bach.de>
2026-09-25 15:00:17 +0200

    BUILD

--- build/calendar-week/index.asset.php
+++ build/calendar-week/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8aa66ed2a07f52de72d5');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '45c82a02ad240d5c28fb');
