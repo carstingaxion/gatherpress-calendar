84c2877c3d44f69bd1fe2e3ff2a1eed0b726f7f7
Carsten Bach <mail@carsten-bach.de>
2026-09-26 09:51:49 +0200

    Specify coloredDot selectors, to not touch any modal content

--- build/calendar-entries/index.asset.php
+++ build/calendar-entries/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => 'f7e9b5130891f3dd990e');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element'), 'version' => '165df7121281480abb30');
